13ff6f9f09ca09a7ec0ba163798ae30ca81b99cf
Wilson Shalo <wilsonshalo@Wilsons-MacBook-Pro.local>
2020-08-21 16:28:39 -0500

    added

--- src/components/InnerSection/ComingSoon.js
+++ src/components/InnerSection/ComingSoon.js
@@ -6,19 +6,63 @@ import Subscribe from "../SubscribeSection/Subscribe";
 const initData = {
   heading: "We are coming soon!",
   content:
-    "The world's most imaginative marketplace. Shaloz is an online marketplace, where people come together to sell and buy products and services. We have millions of one-of-a-kind products and services, so you can find whatever you need. Your one stop marketplace for everything products and services. Looking for a home decor or are you looking for home supplies? Shaloz has it all. No monthly subscription",
+    "The world's most imaginative marketplace. Shaloz is an online marketplace, where people come together to sell and buy products and services. We have millions of one-of-a-kind products and services, so you can find whatever you need. Your one stop marketplace for everything products and services. Looking for a professional home decorator or are you looking for home supplies? Shaloz has it all. No monthly subscription",
   btnText: "Notify",
 };
 
 class ComingSoon extends Component {
   state = {
     initData: [],
+    inputValue: "",
+    subscribeSuc: false,
+    error: false,
   };
   componentDidMount() {
     this.setState({
       initData: initData,
     });
   }
+
+  validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  handleSubscribe = async () => {
+    if (this.state.inputValue !== "") {
+      if (this.validateEmail(this.state.inputValue)) {
+        this.setState({
+          inputValue: "",
+          subscribeSuc: true,
+        });
+
+        setTimeout(() => {
+          this.setState({
+            inputValue: "",
+            subscribeSuc: false,
+          });
+        }, 2000);
+        await fetch("https://quiet-sierra-10781.herokuapp.com/api/subscribe", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            inputValue: this.state.inputValue,
+          }),
+        });
+      } else {
+        this.setState({
+          error: true,
+        });
+        setTimeout(() => {
+          this.setState({
+            error: false,
+          });
+        }, 2000);
+      }
+    }
+  };
   render() {
     return (
       <div className="homepage-3 inner inner-pages">
@@ -49,11 +93,29 @@ class ComingSoon extends Component {
                         type="email"
                         className="form-control"
                         placeholder="info@yourmail.com"
+                        value={this.state.inputValue}
+                        onChange={(e) =>
+                          this.setState({
+                            inputValue: e.target.value,
+                          })
+                        }
                       />
-                      <button type="submit" className="btn btn-bordered">
+                      <button
+                        type="submit"
+                        className="btn btn-bordered"
+                        onClick={this.handleSubscribe}>
                         {this.state.initData.btnText}
                       </button>
                     </div>
+                    {this.state.error && (
+                      <p className="text-white my-4">Your email is not valid</p>
+                    )}
+                    {this.state.subscribeSuc && (
+                      <p className="text-white my-4">
+                        Thanks for subscribing. When Shaloz goes live, you will
+                        be notified.
+                      </p>
+                    )}
                   </div>
                 </div>
               </div>
--- src/components/SubscribeSection/Subscribe.js
+++ src/components/SubscribeSection/Subscribe.js
@@ -1,43 +1,112 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const initData = {
-    heading: "Subscribe to get updates",
-    text: "",
-    submitText: "Subscribe"
-}
+  heading: "Subscribe to get updates",
+  text: "",
+  submitText: "Subscribe",
+};
 
 class Subscribe extends Component {
-    state = {
-        initData: {}
-    }
-    componentDidMount(){
+  state = {
+    initData: {},
+    inputValue: "",
+    subscribeSuc: false,
+    error: false,
+  };
+  validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+  handleSubscribe = async (e) => {
+    e.preventDefault();
+    if (this.state.inputValue !== "") {
+      if (this.validateEmail(this.state.inputValue)) {
         this.setState({
-            initData: initData
-        })
+          inputValue: "",
+          subscribeSuc: true,
+        });
+
+        setTimeout(() => {
+          this.setState({
+            inputValue: "",
+            subscribeSuc: false,
+          });
+        }, 2000);
+        await fetch("https://quiet-sierra-10781.herokuapp.com/api/subscribe", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            inputValue: this.state.inputValue,
+          }),
+        });
+      } else {
+        this.setState({
+          error: true,
+        });
+        setTimeout(() => {
+          this.setState({
+            error: false,
+          });
+        }, 2000);
+      }
     }
-    render() {
-        return (
-            <section className="section subscribe-area ptb_100">
-                <div className="container">
-                    <div className="row justify-content-center">
-                    <div className="col-12 col-md-10 col-lg-7">
-                        <div className="subscribe-content text-center">
-                        <h2>{this.state.initData.heading}</h2>
-                        <p className="my-4">{this.state.initData.text}</p>
-                        {/* Subscribe Form */}
-                        <form className="subscribe-form">
-                            <div className="form-group">
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email" />
-                            </div>
-                            <button type="submit" className="btn btn-lg btn-block">{this.state.initData.submitText}</button>
-                        </form>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  };
+  componentDidMount() {
+    this.setState({
+      initData: initData,
+    });
+  }
+  render() {
+    return (
+      <section className="section subscribe-area ptb_100">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-10 col-lg-7">
+              <div className="subscribe-content text-center">
+                <h2>{this.state.initData.heading}</h2>
+                <p className="my-4">{this.state.initData.text}</p>
+                {/* Subscribe Form */}
+                <form className="subscribe-form">
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter your email"
+                      value={this.state.inputValue}
+                      onChange={(e) =>
+                        this.setState({
+                          inputValue: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-lg btn-block"
+                    onClick={this.handleSubscribe}>
+                    {this.state.initData.submitText}
+                  </button>
+                </form>
+                {this.state.error && (
+                  <p className="text-black my-4">Your email is not valid</p>
+                )}
+                {this.state.subscribeSuc && (
+                  <p className="text-black my-4">
+                    Thanks for subscribing. When Shaloz goes live, you will be
+                    notified.
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Subscribe;
